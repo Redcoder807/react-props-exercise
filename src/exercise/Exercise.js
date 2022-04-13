b47c0bf65ce39f7fe8faf0e82fe717e507fccaf4
Redcoder807 <gearonhaynes@gmail.com>
2022-04-13 14:19:17 -0500

    Done non Bonus

---
+++ src/exercise/Exercise.js
@@ -0,0 +1,10 @@
+function Exercise({good="Great",  bad="Not Great"}) {
+    return(
+        <div>
+        <h1>{good}</h1>
+        <h1>{bad}</h1>
+        </div>
+    );
+}
+
+export default Exercise;
