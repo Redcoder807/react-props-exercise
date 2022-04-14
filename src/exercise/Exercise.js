a9bcdb2790a2ce49eaa90593247793f8c38a3880
Redcoder807 <gearonhaynes@gmail.com>
2022-04-14 10:45:12 -0500

    Bonus Done

--- src/exercise/Exercise.js
+++ src/exercise/Exercise.js
@@ -1,8 +1,11 @@
+import Bonus from "../bonus/Bonus";
+
 function Exercise({good="Great",  bad="Not Great"}) {
     return(
         <div>
         <h1>{good}</h1>
         <h1>{bad}</h1>
+        <Bonus/>
         </div>
     );
 }
